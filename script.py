08e899f344680ff0ffb0eb1c453bb942c58fca1a
dodopontocom <rodolfotiago@gmail.com>
2023-04-25 20:47:48 -0300

    wip: wip

--- script.py
+++ script.py
@@ -8,13 +8,16 @@ openai.api_key = os.environ['CHAT_GPT']
 
 prompt = "Hello, world!"
 model = "text-davinci-002"
-response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=5)
+try:
+    response = openai.Completion.create(engine=model, prompt=prompt, max_tokens=5)
 
-if 'choices' in response.keys() and len(response['choices']) > 0:
-    first_choice = response['choices'][0]
-    if 'text' in first_choice.keys():
-        print(first_choice['text'])
+    if 'choices' in response.keys() and len(response['choices']) > 0:
+        first_choice = response['choices'][0]
+        if 'text' in first_choice.keys():
+            print(first_choice['text'])
+        else:
+            print("No 'text' key found in first choice")
     else:
-        print("No 'text' key found in first choice")
-else:
-    print("No 'choices' key found in response")
+        print("No 'choices' key found in response")
+except openai.error.RateLimitError as e:
+    print("Rate limit exceeded.")
