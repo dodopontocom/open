d0f10dd1971dff40249d7f935db539096e40e1a8
Rodolfo Neto <rodolfotiago@gmail.com>
2023-04-25 20:02:25 -0300

    Create script.py

---
+++ .github/workflows/script.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+          
+import os
+import json
+import http_requests
+
+api_key = os.environ['CHAT_GPT']
+url = 'https://api.openai.com/v1/engines/davinci-codex/completions'
+
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': f'Bearer {api_key}'
+}
+
+data = {
+  'prompt': 'Hello, Chat GPT! Can you generate some text for me?',
+  'max_tokens': 100,
+  'temperature': 0.5,
+  'n': 1,
+  'stop': '.'
+}
+
+response = http_requests.post(url, headers=headers, data=json.dumps(data))
+response_json = response.json()
+
+print(response_json['choices'][0]['text'])
